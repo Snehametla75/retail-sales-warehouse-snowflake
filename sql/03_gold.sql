cd63531af7d62e5a474092713ce83be6fd637646
Snehametla75 <snehametla97@gmail.com>
2026-02-26 06:49:18 +0000

    adding

--- sql/03_gold.sql
+++ sql/03_gold.sql
@@ -20,3 +20,27 @@ SELECT
     SUM(sales_price) AS total_sales
 FROM SILVER.FACT_SALES
 GROUP BY sales_year;
+
+CREATE OR REPLACE TABLE GOLD.MONTHLY_SALES AS 
+SELECT 
+    DATE_TRUNC('month',transaction_date) AS sales_month,
+    SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY sales_month;
+
+CREATE OR REPLACE TABLE GOLD.TOP_STORES AS
+SELECT 
+    store_id,
+    SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY store_id
+ORDER BY total_sales DESC
+LIMIT 10; 
+
+
+SELECT COUNT(*) FROM GOLD.MONTHLY_SALES;
+SELECT COUNT(*) FROM GOLD.TOP_STORES;
+SELECT COUNT(*) FROM GOLD.SALES_BY_STATE;
+SELECT COUNT(*) FROM GOLD.SALES_BY_YEAR;
+select sum(sales_price) from silver.fact_sales;
+select sum(total_sales) from gold.sales_by_state;
